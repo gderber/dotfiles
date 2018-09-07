25a99ea8a19c8f2c0025aab21e3bdab780f65376
G S Derber <gd.smlinux@gmail.com>
2018-09-07 16:57:18 -0400

    Remove elpa files

--- src/emacs.d/elpa/dash-20180726.1213/dash-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("../../../../../../../.emacs.d/elpa/dash-20180726.1213/dash-autoloads.el"
-;;;;;;  "../../../../../../../.emacs.d/elpa/dash-20180726.1213/dash.el")
-;;;;;;  (23425 60298 253092 862000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
