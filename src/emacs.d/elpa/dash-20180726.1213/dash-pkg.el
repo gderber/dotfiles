ef974f4996e645dcecfafd9b4db40e8f635204ee
G S Derber <gd.smlinux@gmail.com>
2018-09-07 12:35:46 -0400

    Remove elpa files

--- src/emacs.d/elpa/dash-20180726.1213/dash-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dash" "20180726.1213" "A modern list library for Emacs" 'nil :authors '(("Magnar Sveen" . "magnars@gmail.com")) :maintainer '("Magnar Sveen" . "magnars@gmail.com"))
