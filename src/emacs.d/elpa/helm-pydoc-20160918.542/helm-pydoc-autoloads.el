f8b4fc70ec3e8bf0cea35646ac88f2e5ca476a65
G Derber <gd.github@bloodymage.org>
2018-09-07 12:35:46 -0400

    Remove elpa files

--- src/emacs.d/elpa/helm-pydoc-20160918.542/helm-pydoc-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; helm-pydoc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "helm-pydoc" "helm-pydoc.el" (23426 62614 303352
-;;;;;;  698000))
-;;; Generated autoloads from helm-pydoc.el
-
-(autoload 'helm-pydoc "helm-pydoc" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("helm-pydoc-pkg.el") (23426 62613 95349
-;;;;;;  203000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-pydoc-autoloads.el ends here
