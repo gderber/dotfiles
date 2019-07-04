fa0d80f585fde4102d452841399c79ac1a313f6a
Thomas Anderson <neo@drbr.org>
2019-07-04 09:01:59 -0400

    Enable layers

--- src/spacemacs.d/private/drbr-common/funcs.el
+++ src/spacemacs.d/private/drbr-common/funcs.el
@@ -45,6 +45,55 @@
 ;; 
 ;;; Code:
 
+(defun drbr-common/insert-date (prefix)
+;;  "Insert the current date. With prefix-argument, use ISO format. With
+;;   two prefix arguments, write out the day and month name."
+  (interactive "P")
+  (let ((format (cond
+		 ((not prefix) "%d.%m.%Y")
+		 ((equal prefix '(4)) "%Y-%m-%d")
+		 ((equal prefix '(16)) "%A, %d. %B %Y")))
+	(system-time-locale "de_DE"))
+    (insert (format-time-string format))))
+
+(defun drbr-common/fix-comments ()
+  "Move through the entire buffer searching for comments that begin with
+    \"//\" and fill them appropriately.  That means if comments start too
+    close to the end of line (20 less than the fill-column) move the
+    entire comment on a line by itself."
+  (interactive)
+  (save-excursion
+    (beginning-of-buffer)
+    (while (search-forward "//")
+      (lisp-indent-for-comment)
+      ;; when the comment is deemed to be too close to the end of the
+      ;; line, yank it and put it on the previous line before filling
+      (while (< (- fill-column 20) (- (current-column) 3))
+       (search-backward "//")
+       (kill-line)
+       (beginning-of-line)
+       (yank)
+       (insert "\n"))
+      ;; now fill the lines that are too long
+      (if (and (not (end-of-line))
+	       (< fill-column (current-column)))
+	  (c-fill-paragraph)))))
+
+;; Supply random fortune cookie
+(defun drbr-common/fortune-scratch-message()
+  (interactive)
+  (let ((fortune
+         (when (executable-find "fortune")
+           (with-temp-buffer
+             (shell-command "fortune" t)
+             (while (not (eobp))
+               (insert ";;")
+               (forward-line))
+             (delete-trailing-whitespace (point-min) (point-max))
+             (concat (buffer-tring) "\n")))))
+    (if (called-interactivly-p 'any)
+        (insert fortune)
+      fortune)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
